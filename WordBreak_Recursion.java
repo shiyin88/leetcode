f22674958ed59b91009b82c67373674515bd92d0
shiyin88 <shiyin88@gmail.com>
2014-09-08 15:09:31 -0400

    wordBreak   exceeded time limit

---
+++ WordBreak_Recursion.java
@@ -0,0 +1,41 @@
+/**
+*Given a string s and a dictionary of words dict,
+determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+Time: O(2^n)
+*/
+
+public class Solution {
+    public boolean wordBreak(String s, Set<String> dict) {
+        return wordBreakHelper(s, dict, 0);
+
+    }
+
+    public boolean wordBreakHelper(String s, Set<String> dict, int start){
+       if (s.length() == start){
+          return true;
+        }
+
+        for (String str : dict){
+
+          int end = start + str.length();
+          if (end > s.length()){
+        	  continue;
+          }
+
+          if (s.substring(start, end).equals(str)){
+
+             if (wordBreakHelper(s, dict, end)){
+            	 return true;
+             }
+          }
+        }
+
+        return false;
+    }
+}
