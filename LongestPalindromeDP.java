8ef52c2981568a7db09ce5724b1f57edd12547d2
shiyin88 <shiyin88@gmail.com>
2014-09-11 21:04:45 -0400

    minor change

--- LongestPalindromeDP.java
+++ LongestPalindromeDP.java
@@ -6,7 +6,7 @@ public class Solution {
             return null;
           }
           //1. if s is a single letter
-          if (s.length() <= 1){
+          if (s.length() == 1){
             return s;
           }
 
@@ -21,7 +21,7 @@ public class Solution {
 
           // 2. check for sub-string of length 2.
           for (int i = 0; i< s.length() - 1; i++){
-            if (s.charAt( i) == s.charAt(i + 1)){
+            if (s.charAt(i) == s.charAt(i + 1)){
               table[i][i + 1] = 1;
               longStr = s.substring(i,i + 2);//[i,i+1]
             }
