38d4b44e7365f181405f051c23d0e31b60555306
shiyin88 <Shiyin@172-26-206-15.DYNAPOOL.NYU.EDU>
2014-10-16 19:29:07 -0400

    update on 10/16 Binary Search Tree

--- Binary Search/72_SearchInA2DMatrix.java
+++ Binary Search/72_SearchInA2DMatrix.java
@@ -22,13 +22,18 @@ public class Solution {
         int start = 0;
         int end = row * col - 1;
         int mid = 0;
-        while (start <= end){
+        while (start <= end){//由于难以在matrix中定位start和end的具体值
+          //所以这里我们用start <= end而不是start + 1 < end来定义
+            //如果把所有的数排成一个一维数组的话，这里mid是一维数组的mid index
             mid = start + (end - start)/2;
+            //此为中间值
             int digit = matrix[mid/col][mid%col];
+            //下面也是按照binary Search 求值
             if (target == digit){
                 return true;
             }else if (target < digit){
-                end = mid - 1;
+                end = mid - 1;//由于while 循环是start <= end
+                //为了避免跳不出loop end要设为mid - 1 否则比如{1，1}end则永远为1跳不出死循环
             }else if (target > digit){
                 start = mid + 1;//plus 1 or minus one to avoid ifinite loop as the condition for the while loop is (start<=end)
             }
