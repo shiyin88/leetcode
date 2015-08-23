c846a27efd39a0d29af2b53b380395e9e387c7b6
shiyin88 <shiyin88@gmail.com>
2015-08-23 16:23:49 -0700

    Update 134_M_GasStation.java

--- Array/134_M_GasStation.java
+++ Array/134_M_GasStation.java
@@ -15,8 +15,10 @@ Hide Tags Greedy
  * 分别初始化两个variable ： sum & total
  * sum : 记录当前gas[i] - cost[i] 是否< 0. < 0则表示为从之前的index走到当前i时： 损耗大于收入。则需要重新赋值为0
  *       重新记录新的点开始走。此时，由于当i时，不能走circuit，所以把maxIndex 赋值为 i + 1。假设下一个点可以走circuit
- * total : 记录整个旅程中总的支出和收入。total的意义在于：如果> 0表示必有一点开始，可以走circuit。如果total < 0则无力从哪里
- * 走都无法走circuit
+ * total : 记录整个旅程中总的支出和收入。total的意义在于：如果> 0，则表示gas有剩余，肯定能走一圈。但是我们不知道从哪个点走可以走一圈。
+ *          所以，我们用sum来记录某点开始后的盛或是负于的情况。其从某点（即index来记录）开始，sum一直>0，可以断定index表示其可以走一圈
+ *          的点。
+ *          如果<0 走都无法走circuit
  * */
 public class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
