d310c3aaea362a83cafb1cf50dea8dacea642499
shiyin88 <shiyin88@gmail.com>
2015-08-23 16:31:47 -0700

    Update 121_M_BestTimeToBuyAndSellStockI.java

--- Dynamic Programming/121_M_BestTimeToBuyAndSellStockI.java
+++ Dynamic Programming/121_M_BestTimeToBuyAndSellStockI.java
@@ -2,6 +2,8 @@
 
 If you were only permitted to complete at most one transaction
 (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+这里，在所以的transaction中，只能交易一次（买和卖）
+即，要在所以值中找寻minBuy and maxSell;
 
 有两个变量我们应该keep track of
 1) 买入价
