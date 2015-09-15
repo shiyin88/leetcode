775e74e0d8eaa8db3f52275715c4332d1522c5d5
shiyin88 <shiyin88@gmail.com>
2015-09-14 22:49:23 -0700

    Update 90_SubsetsII.java

--- DFS/90_SubsetsII.java
+++ DFS/90_SubsetsII.java
@@ -30,16 +30,18 @@ public class Solution {
 
 		private static void subsetsWithDupHelper(ArrayList<ArrayList<Integer>> result,
 				ArrayList<Integer> list, int[] num, int position) {
-			result.add(new ArrayList<Integer>(list));
+			if (!result.contains(path)){
+				result.add(new ArrayList<Integer>(list));
+			}		
 	 //只在回溯的时候比较i 和 i-1的值。ex 回溯到i=2， 必然i=2下面有i=3，i=4. 2即为position
    //为回溯到2的时候 的起点。当i++ 到 3 或是 4的时候，需要和起点position = 2 来比较，如果
    //值相同，即continue
    //i = pos的时候是执行第一个if statement的时候，是起始位置。确保在起始的时候，用了num[i]
    //的值，然后当再从num中抽取i的值时，保证后面的值和前一个值不一样。就不会有重复
 			for (int i = position; i < num.length; i++) {
-				if (i != position && num[i] == num[i-1]){
-					continue;
-				}
+				//if (i != position && num[i] == num[i-1]){
+				//	continue;
+				//}
 
 				list.add(num[i]);
 				subsetsWithDupHelper(result, list, num, i+1);
