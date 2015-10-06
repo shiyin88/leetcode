c9d41714e7958b892865f470445c9f046278a435
shiyin88 <shiyin88@gmail.com>
2015-10-05 23:12:10 -0700

    Update 105_M_ConstructBinaryTreefromPreorderandInorderTraversal.java

--- Binary Tree/105_M_ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ Binary Tree/105_M_ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -19,6 +19,18 @@ Hide Tags Tree Array Depth-first Search
  * Inorder :    left : i - 1; right : i + 1
  * Preorder :   left : i - instart + pstart
  *              right : i - instart + pstart + 1
+ *              
+ * 
+ * in-order:   4 2 5 (1) 6 7 3 8
+   pre-order: (1) 2 4 5  3 7 6 8
+   由于preorder是先traverse root， 所以preorder中第一个数为root，即1为root
+   然后我们iterate in-order的数列，找到root在inorder中所在的index i
+   (**通过root所在的index，我们知道BST的左支点共有i - 1个index的叶子，右支点共有i+1个index的叶子)
+   在新建的binary tree中，我们可知，inorder.left = root的左半部分的index, 即 [0, i - 1]
+                                    inorder.right = root的右半部分, 即 [i + 1, inorder.length - 1]
+    preorder 的left为: [pstart + 1, pstart + i - istart]
+    preorder 的right为: [pstart + i - istart + 1, pend]
+                                    
  */
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
